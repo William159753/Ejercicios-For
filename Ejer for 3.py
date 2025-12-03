03c2d14deec8b6546dbfff1f7329c5550b39f40f
William159753 <chonatawilliam@gmail.com>
2025-12-02 21:33:45 -0500

    Firs

---
+++ Ejer for 3.py
@@ -0,0 +1,6 @@
+numero= int (input("Ingrese un numero: "))
+print ("Tabla", numero)
+for i in range(12):
+    i=i+1
+    mult=(numero * i)
+    print (numero, "x", i,"=",mult )
---
+++ Ejerc for 2.py
@@ -0,0 +1,7 @@
+suma=0
+for i in range(10):
+    calificacion = float(input("Ingrese la calificacion: "))
+    suma = suma + calificacion
+promedio = suma/10
+print ("El sumatorio es: ", suma)
+print ("El promedio total es: ", promedio)
